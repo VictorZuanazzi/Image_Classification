c078e15adc92cce03da61a2d9cad67dd4f31d819
Victor <victorzuanazzi@gmail.com>
2019-03-14 15:49:42 +0100

    Added SVMs

--- main.m
+++ main.m
@@ -11,7 +11,7 @@ im_dim = [96,96,3];
 % Set parameters
 image_type = "gray";
 sampling_strategy = "key point";
-vocabulary_size = 100;
+vocabulary_size = 400;
 
 %magic function that loads the images and reshapes them
 [x_train, y_train] = load_n_reshape(train_path, keep, im_dim);
@@ -25,5 +25,25 @@ imshow(x_train{1})
 
 % Create vocabulary
 vocabulary = create_vocabulary(x1, sampling_strategy, image_type, vocabulary_size);
-BoW = BoW_representation(x2, "key point", "gray", vocabulary, false);
-   
+
+% Create the BoW for all images
+x_svm_BoW = BoW_representation(x_svm, sampling_strategy, image_type, vocabulary, false);
+x_test_BoW = BoW_representation(x_test, sampling_strategy, image_type, vocabulary, false);
+
+%creates binary label for the class of interest.
+Y = y_svm == 1;
+
+%train the SVM
+SVMModel = fitcsvm(x_svm_BoW, Y');
+
+%make one prediction
+[label, score] = predict(SVMModel, x_test_BoW(1,:));
+imshow(x_test{1});
+disp(label)
+dis(score)
+
+
+
+
+
+
